3b0b0cbeeb5c399f91e11c70d68a162db69190b4
iago1460 <1460.i.iago@gmail.com>
2014-02-20 13:30:33 +0100

    Added BSD License.

--- radio/settings.py
+++ radio/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = (
+    'bootstrap_admin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
