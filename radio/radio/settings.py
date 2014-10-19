bf78fbf8b49a97ba828d0977d5f82b0f7e1c18c1
iago1460 <1460.i.iago@gmail.com>
2014-10-19 17:55:55 +0200

    Changed DB to sqlite in development. Updated requirements.

--- radio/radio/settings.py
+++ radio/radio/settings.py
@@ -115,12 +115,8 @@ WSGI_APPLICATION = 'radio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE':'django.db.backends.mysql',
-        'NAME': 'radio',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
