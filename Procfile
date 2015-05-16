914aa8caf23ef84b14e63f3de517dc407137fd2e
iago1460 <1460.i.iago@gmail.com>
2015-05-16 21:53:20 +0100

    Changed project structure

--- Procfile
+++ Procfile
@@ -1,3 +1,1 @@
 web: gunicorn --pythonpath radio radio.configs.heroku.wsgi --log-file -
-
-heroku ps:scale web=1
--- fabfile.py
+++ fabfile.py
@@ -10,7 +10,9 @@ def heroku_setup():
     local('heroku create %(heroku_project_name)s' % env)
     local('git init')
     local('heroku git:remote -a %(heroku_project_name)s' % env)
+    local('heroku ps:scale web=1')
 
+def commit_change
 
 def master():
     """
@@ -19,12 +21,30 @@ def master():
     env.branch = 'master'
 
 
+def commit():
+    local('git add .')
+    local('git commit -am "autocommit"')
+
+
 def heroku_deploy():
-    require('branch', provided_by=[master])
+    # Save current branch
+    env.current_branch = local('git rev-parse --abbrev-ref HEAD', capture=True)
 
-    # local('git add .')
-    # local('git commit -am "autocommit to deploy"')
+    # Change branch
+    local('git checkout -b local_heroku')
 
+    # Add local settings
+    local('git add -f radio/configs/common/local_settings.py')
+    local('git add -f radio/configs/development/local_settings.py')
+    local('git add -f radio/configs/heroku/local_settings.py')
+    local('git commit -m "autocommit: Added local_settings"')
+
+    # Deploy changes
     local('git push heroku %(branch)s' % env)
+
+    # Install requirements and run migrations
     local('heroku run "pip install -r %(heroku_config_path)s/requirements.txt"' % env)
     local('heroku run "python %(heroku_config_path)s/manage.py migrate"' % env)
+
+    # Return current branch
+    local('git checkout %(current_branch)s' % env)
