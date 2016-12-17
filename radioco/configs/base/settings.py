fcff3eb2f428cddd41fe527c6d729d06842e5db8
Iago Veloso <iago.veloso@thisisglobal.com>
2016-12-17 08:23:37 +0000

    Removing django-bower dependency and using bower directly

--- radioco/configs/base/settings.py
+++ radioco/configs/base/settings.py
@@ -48,7 +48,6 @@ INSTALLED_APPS = (
 
     'grappelli',
     'filebrowser',
-    'djangobower',
 
     'django.contrib.admin',
     'django.contrib.sitemaps',
@@ -149,21 +148,10 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(SITE_ROOT, 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(SITE_ROOT, 'media')
-BOWER_COMPONENTS_ROOT = os.path.join(SITE_ROOT, 'components')
-
-BOWER_PATH = os.path.join(SITE_ROOT, '..', 'node_modules', '.bin', 'bower')
-
-BOWER_INSTALLED_APPS = (
-    'jqueryui#1.11.4',
-    'jquery#2.2.1',
-    'bootstrap#3.3.6',
-    'fullcalendar#2.8.0',
-)
 
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-    'djangobower.finders.BowerFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder'
 )
 
 SITE_ID = 1
