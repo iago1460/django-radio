f6aa6a004eab42075696fdaff9aa86c812634b5d
Iago Veloso <iago.veloso@thisisglobal.com>
2017-06-01 19:57:09 +0100

    Avoiding create example data if db is not empty

--- radioco/apps/radioco/management/commands/create_example_data.py
+++ radioco/apps/radioco/management/commands/create_example_data.py
@@ -1,8 +1,12 @@
 from django.core.management.base import BaseCommand
 
+from radioco.apps.programmes.models import Programme
 from radioco.apps.radioco.utils import create_example_data
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        create_example_data()
+        if Programme.objects.exists():
+            print('There is already some data in the db, skipping creation...')
+        else:
+            create_example_data()
