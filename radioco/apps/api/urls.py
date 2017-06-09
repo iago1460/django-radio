47225c16736979093c40aaf5b901cf8c0af69ce3
Iago Veloso <iago.veloso@thisisglobal.com>
2017-06-09 18:23:20 +0100

    Fixing API router

--- radioco/apps/api/urls.py
+++ radioco/apps/api/urls.py
@@ -11,9 +11,9 @@ router.register(r'transmissions', views.TransmissionViewSet, base_name='transmis
 router.register(r'operations', views.TransmissionOperationViewSet, base_name='operation')
 
 radiocom_router = routers.DefaultRouter(trailing_slash=False)
-radiocom_router.register(r'programmes', views.RadiocomProgrammeViewSet)
-radiocom_router.register(r'transmissions', views.RadiocomTransmissionViewSet, base_name='transmission')
-radiocom_router.register(r'radiostation', views.RadiocomStation, base_name='radiostation')
+radiocom_router.register(r'programmes', views.RadiocomProgrammeViewSet, base_name='radiocom_programmes')
+radiocom_router.register(r'transmissions', views.RadiocomTransmissionViewSet, base_name='radiocom_transmission')
+radiocom_router.register(r'radiostation', views.RadiocomStation, base_name='radiocom_radiostation')
 
 urlpatterns = router.urls + [
     url(r'^radiocom/', include(radiocom_router.urls)),
