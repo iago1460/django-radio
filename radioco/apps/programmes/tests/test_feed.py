01177d4f875adb928c16be03e078eb82328f5d18
Iago Veloso <iago.veloso@thisisglobal.com>
2017-06-18 22:03:14 +0100

    Fixing test

--- radioco/apps/programmes/tests/test_feed.py
+++ radioco/apps/programmes/tests/test_feed.py
@@ -13,7 +13,7 @@ EXPECTED_RESULT = '''<?xml version="1.0" encoding="utf-8"?>
         Lorem Ipsum is simply dummy text of the printing and typesetting industry.
         Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
         when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-    </itunes:summary><itunes:image>http://testserver/media/_versions/defaults/example/radio_1_itunes_image.jpg</itunes:image><itunes:category>News &amp; Politics</itunes:category><image url="http://testserver/media/_versions/defaults/example/radio_1_rss_image.jpg" link="/programmes/morning-news/" title="Morning News"></image><item><title>1x1 Episode 1</title><link>http://example.com/programmes/morning-news/1x1/</link><description>
+    </itunes:summary><itunes:image href="http://testserver/media/_versions/defaults/example/radio_1_itunes_image.jpg"></itunes:image><itunes:category text="News &amp; Politics"></itunes:category><image url="http://testserver/media/_versions/defaults/example/radio_1_rss_image.jpg" link="http://testserver/programmes/morning-news/" title="Morning News"></image><item><title>1x1 Episode 1</title><link>http://example.com/programmes/morning-news/1x1/</link><description>
         Lorem Ipsum is simply dummy text of the printing and typesetting industry.
         Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
         when an unknown printer took a galley of type and scrambled it to make a type specimen book.
