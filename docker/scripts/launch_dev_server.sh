e6b6a669b524bba8809c919a10c591dc00009e6e
Chrysweel <festixeiro@gmail.com>
2024-12-01 00:20:49 +0100

    remove change of launch_dev_server, it is another PR

--- docker/scripts/launch_dev_server.sh
+++ docker/scripts/launch_dev_server.sh
@@ -3,7 +3,6 @@
 script_full_path=$(dirname "$0")
 $script_full_path/wait_for.sh postgres 5432
 
-rm -rf /radioco/radioco/apps/radioco/static/bower
 mv -f /tmp_bower /radioco/radioco/apps/radioco/static/bower
 python3 manage.py migrate
 python3 manage.py runserver 0.0.0.0:8000
